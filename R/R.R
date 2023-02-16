e99cde6b7a120156b6093b8fc2461bd41d62170f
Reed W Priest <pries153@umn.edu>
2023-02-15 19:12:11 -0600

    Steps 1:10 done.

--- R/R.R
+++ R/R.R
@@ -5,6 +5,16 @@ library(lubridate)
 
 #Data Import
 Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-Anotes_tbl <- read_delim(file = "../data/Anotes.csv", col_names = c("parnum", "notes"))
+Anotes_tbl <- read_delim(file = "../data/Anotes.csv", col_names = c("parnum", "notes"), skip = 1)
 Bdata_tbl <- read_tsv(file = "../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
 Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", col_names = c("parnum", "notes"))
+
+#Data Cleaning
+Aclean_tbl <- Adata_tbl %>%
+  separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = " - ") %>%
+  mutate(datadate = parse_date_time(Adata_tbl$datadate, c("%b %d %Y, %H:%M:%S"))) %>%
+  mutate(across(contains("q"), ~ as.integer(.))) %>%
+  full_join(Anotes_tbl, by = "parnum") %>%
+  subset(is.na(notes))
+
+
