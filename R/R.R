31000718091abf1f35ed1d441b98421fea19b9aa
Reed W Priest <pries153@umn.edu>
2023-02-15 19:20:10 -0600

    Steps 1:11 almost done (need to finish Line 25).

--- R/R.R
+++ R/R.R
@@ -7,7 +7,7 @@ library(lubridate)
 Adata_tbl <- read_delim(file = "../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_delim(file = "../data/Anotes.csv", col_names = c("parnum", "notes"), skip = 1)
 Bdata_tbl <- read_tsv(file = "../data/Bparticipants.dat", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
-Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", col_names = c("parnum", "notes"))
+Bnotes_tbl <- read_delim(file = "../data/Bnotes.txt", col_names = c("parnum", "notes"), skip = 1)
 
 #Data Cleaning
 Aclean_tbl <- Adata_tbl %>%
@@ -16,5 +16,9 @@ Aclean_tbl <- Adata_tbl %>%
   mutate(across(contains("q"), ~ as.integer(.))) %>%
   full_join(Anotes_tbl, by = "parnum") %>%
   subset(is.na(notes))
-
-
+ABclean_tbl <- Bdata_tbl %>%
+  mutate(datadate = parse_date_time(Bdata_tbl$datadate, c("%b %d %Y, %H:%M:%S"))) %>%
+  mutate(across(contains("q"), ~ as.integer(.))) %>%
+  full_join(Bnotes_tbl, by = "parnum") %>%
+  subset(is.na(notes)) %>%
+  select(casenum:q10)
